fcb99dab716a7ff3a219c3eea92c7e5d38c7577f
Maria Cecilia Caporale <cecicaporale16@gmail.com>
2025-10-15 16:54:29 -0300

    refactor: reorganizar config do tailwind, gitignore e melhorias no layout e globals css

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,36 @@
+import '../globals.css'
+import { Inter, Chakra_Petch } from 'next/font/google'
+import { ReactNode } from 'react'
+import { Header } from '../../components/organisms/Header'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-inter',
+})
+const chakraPetch = Chakra_Petch({
+  subsets: ['latin'],
+  weight: ['700'],
+  variable: '--font-chakra-petch',
+})
+
+export const metadata = {
+  title: 'Fernanda Mascheti',
+  description: 'pensar no que colocar aqui',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="pt-BR" className={`${inter.variable} ${chakraPetch.variable}`}>
+      <body className="bg-background text-foreground">
+        <div className="flex min-h-screen flex-col">
+          <Header />
+          <main className="flex-grow px-8 py-6 sm:px-20">
+            {children}
+          </main>
+        </div>
+      </body>
+    </html>
+  )
+}
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,8 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        card: 'hsl(var(--card))',
-        'card-foreground': 'hsl(var(--card-foreground))',
-        primary: 'hsl(var(--primary))',
-        'primary-foreground': 'hsl(var(--primary-foreground))',
-        'muted-foreground': 'hsl(var(--muted-foreground))',
-      },
-      fontFamily: {
-        sans: ['var(--font-inter)', 'sans-serif'],
-        heading: ['var(--font-chakra-petch)', 'sans-serif'],
-      },
-      fontSize: {
-        base: ['16px', '150%'],
-        lg: ['20px', '100%'],
-        xl: ['24px', '100%'],
-        '2xl': ['60px', '101%'],
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+  content: ['./src/**/*.{ts,tsx,js,jsx}'],
+  darkMode: 'class',
+}
+
+export default config
