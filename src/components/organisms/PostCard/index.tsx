29d69b9a1001e48b94ec7802bf758ed87b2e3216
Maria Cecilia Caporale <cecicaporale16@gmail.com>
2025-10-16 17:28:33 -0300

    fear: novo componente postCard

---
+++ src/components/organisms/PostCard/index.tsx
@@ -0,0 +1,60 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import type { FullPost } from "@/app/types";
+import { CardTag } from "../../atoms/CardTag";
+
+interface CardProps {
+  post: FullPost;
+}
+
+export default function PostCard({ post }: CardProps) {
+  const cleanImg = post.imageUrl ? post.imageUrl.split("?")[0] : "";
+
+  return (
+    <div
+        className="
+        relative
+        w-full
+        max-w-[381px] 
+        h-auto 
+        bg-primary-foreground
+        rounded-[4px]
+        p-6
+        border
+        border-primary
+        group
+        transition-transform
+        duration-300
+        hover:shadow-[0_4px_44px_0_rgba(28,167,200,0.3)]
+        "
+      >
+        <div className="w-full h-[196px] relative mb-4">
+          {cleanImg && (
+            <Image
+              src={cleanImg}
+              alt={`Imagem de capa do post: ${post.title}`}
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="object-cover w-full h-auto"
+            />
+          )}
+          <CardTag text={post.category.name} />
+        </div>
+
+        <h1 className="text-foreground font-bold text-[20px] font-heading line-clamp-2 mb-2">
+          {post.title}
+        </h1>
+        <p className="text-muted-foreground text-[16px] line-clamp-4">
+          {post.content}
+        </p>
+        <Link 
+          href={`/blog/${post.id}`} 
+          className="text-primary text-[16px] font-bold hover:underline inline-block mt-4"
+        >
+          Ler mais
+        </Link>
+      </div>
+  );
+}
+
