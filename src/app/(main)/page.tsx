b671bc3e6ad23912c94171c2d273920e8ea85e0b
Maria Cecilia Caporale <cecicaporale16@gmail.com>
2025-10-17 10:29:39 -0300

    chore: console log removido

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,20 +1,17 @@
 import { HeroSection } from "../../components/organisms/HeroSection";
 import { ContactSection } from "../../components/organisms/ContactSection";
 import { BlogSection } from "../../components/organisms/BlogSection";
-import { getPosts, getAllPosts } from "../services/posts.service";
+import { getAllPosts } from "../services/posts.service";
 import { FullPost } from "../types";
 
 export default async function Home() {
-  const initialData = await getPosts(1, 6);
   const allPosts: FullPost[] = await getAllPosts();
-console.log(">>>>>>>:", initialData);
+
   return (
     <div className="min-h-screen">
       <main className="container mx-auto">
         <HeroSection />
-        
        <BlogSection initialPosts={allPosts} />
-        
       </main>
       <ContactSection />
     </div>
